a4964094ffef407d8d0dac517bf1652e6b09a12a
KonagallaSurya <suryakonagalla01@gmail.com>
2026-08-08 14:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (55.87%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.py
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.py
@@ -2,7 +2,7 @@ class Solution:
     def isFascinating(self, n: int) -> bool:
         a=2*n
         b=3*n
-        out=str(a)+str(b)+str(n)
+        out=str(n)+str(a)+str(b)
         if len(out)!=9:
             return False
         for i in range(1,10):
